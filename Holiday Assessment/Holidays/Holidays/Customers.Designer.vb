8efaa8b0f75eda67733cfd60a01183878bde767d
elliotm16 <elliotdog2@sky.com>
2017-03-30 11:06:40 +0100

    Add files via upload

---
+++ Holiday Assessment/Holidays/Holidays/Customers.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Customers
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.chkSaveMode = New System.Windows.Forms.CheckBox()
+        Me.cmdSave = New System.Windows.Forms.Button()
+        Me.lblName = New System.Windows.Forms.Label()
+        Me.txtPhoneNumber = New System.Windows.Forms.TextBox()
+        Me.txtEmailAddress = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtCustomerID = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cmdSearch = New System.Windows.Forms.Button()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.btnClear)
+        Me.Panel1.Controls.Add(Me.chkSaveMode)
+        Me.Panel1.Controls.Add(Me.cmdSave)
+        Me.Panel1.Controls.Add(Me.lblName)
+        Me.Panel1.Controls.Add(Me.txtPhoneNumber)
+        Me.Panel1.Controls.Add(Me.txtEmailAddress)
+        Me.Panel1.Controls.Add(Me.txtSurname)
+        Me.Panel1.Controls.Add(Me.txtFirstName)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.txtCustomerID)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.cmdSearch)
+        Me.Panel1.Location = New System.Drawing.Point(11, 10)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(323, 320)
+        Me.Panel1.TabIndex = 22
+        '
+        'btnClear
+        '
+        Me.btnClear.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.btnClear.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClear.Location = New System.Drawing.Point(215, 229)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(96, 32)
+        Me.btnClear.TabIndex = 20
+        Me.btnClear.Text = "Clear Textboxes"
+        Me.btnClear.UseVisualStyleBackColor = False
+        '
+        'chkSaveMode
+        '
+        Me.chkSaveMode.AutoSize = True
+        Me.chkSaveMode.Checked = True
+        Me.chkSaveMode.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.chkSaveMode.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.chkSaveMode.Location = New System.Drawing.Point(115, 182)
+        Me.chkSaveMode.Name = "chkSaveMode"
+        Me.chkSaveMode.Size = New System.Drawing.Size(97, 20)
+        Me.chkSaveMode.TabIndex = 19
+        Me.chkSaveMode.Text = "Save Mode"
+        Me.chkSaveMode.UseVisualStyleBackColor = True
+        '
+        'cmdSave
+        '
+        Me.cmdSave.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.cmdSave.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdSave.Location = New System.Drawing.Point(11, 229)
+        Me.cmdSave.Name = "cmdSave"
+        Me.cmdSave.Size = New System.Drawing.Size(96, 32)
+        Me.cmdSave.TabIndex = 18
+        Me.cmdSave.Text = "Save"
+        Me.cmdSave.UseVisualStyleBackColor = False
+        '
+        'lblName
+        '
+        Me.lblName.AutoSize = True
+        Me.lblName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.lblName.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblName.Location = New System.Drawing.Point(6, 6)
+        Me.lblName.Name = "lblName"
+        Me.lblName.Size = New System.Drawing.Size(106, 29)
+        Me.lblName.TabIndex = 9
+        Me.lblName.Text = "Holidays"
+        '
+        'txtPhoneNumber
+        '
+        Me.txtPhoneNumber.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtPhoneNumber.Location = New System.Drawing.Point(109, 131)
+        Me.txtPhoneNumber.Name = "txtPhoneNumber"
+        Me.txtPhoneNumber.Size = New System.Drawing.Size(112, 20)
+        Me.txtPhoneNumber.TabIndex = 5
+        '
+        'txtEmailAddress
+        '
+        Me.txtEmailAddress.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtEmailAddress.Location = New System.Drawing.Point(109, 107)
+        Me.txtEmailAddress.Name = "txtEmailAddress"
+        Me.txtEmailAddress.Size = New System.Drawing.Size(112, 20)
+        Me.txtEmailAddress.TabIndex = 4
+        '
+        'txtSurname
+        '
+        Me.txtSurname.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtSurname.Location = New System.Drawing.Point(109, 83)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(112, 20)
+        Me.txtSurname.TabIndex = 3
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtFirstName.Location = New System.Drawing.Point(109, 59)
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(112, 20)
+        Me.txtFirstName.TabIndex = 2
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(7, 131)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(98, 16)
+        Me.Label5.TabIndex = 14
+        Me.Label5.Text = "Phone Number"
+        '
+        'txtCustomerID
+        '
+        Me.txtCustomerID.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.txtCustomerID.Location = New System.Drawing.Point(109, 35)
+        Me.txtCustomerID.Name = "txtCustomerID"
+        Me.txtCustomerID.Size = New System.Drawing.Size(112, 20)
+        Me.txtCustomerID.TabIndex = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(8, 107)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(96, 16)
+        Me.Label4.TabIndex = 13
+        Me.Label4.Text = "Email Address"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(7, 83)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(62, 16)
+        Me.Label3.TabIndex = 12
+        Me.Label3.Text = "Surname"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(8, 60)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 16)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "First Name"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(8, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(81, 16)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Customer ID"
+        '
+        'cmdSearch
+        '
+        Me.cmdSearch.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.cmdSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cmdSearch.Location = New System.Drawing.Point(113, 229)
+        Me.cmdSearch.Name = "cmdSearch"
+        Me.cmdSearch.Size = New System.Drawing.Size(96, 32)
+        Me.cmdSearch.TabIndex = 0
+        Me.cmdSearch.Text = "Search"
+        Me.cmdSearch.UseVisualStyleBackColor = False
+        '
+        'Customers
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(344, 341)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Customers"
+        Me.Text = "Customers"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+    Friend WithEvents chkSaveMode As System.Windows.Forms.CheckBox
+    Friend WithEvents cmdSave As System.Windows.Forms.Button
+    Friend WithEvents lblName As System.Windows.Forms.Label
+    Friend WithEvents txtPhoneNumber As System.Windows.Forms.TextBox
+    Friend WithEvents txtEmailAddress As System.Windows.Forms.TextBox
+    Friend WithEvents txtSurname As System.Windows.Forms.TextBox
+    Friend WithEvents txtFirstName As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtCustomerID As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Private WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents cmdSearch As System.Windows.Forms.Button
+End Class
---
+++ Holiday Assessment/Holidays/Holidays/Customers.vb
@@ -0,0 +1,290 @@
+﻿Imports System.IO
+
+Public Class Customers
+
+    ' Structure for the customer's data
+    Private Structure Customer
+
+        Public CustomerID As String ' Used to uniquely identify a customer
+        Public FirstName As String
+        Public Surname As String
+        Public EmailAddress As String
+        Public PhoneNumber As String
+
+    End Structure
+
+    Private Sub Holidays_Load() Handles MyBase.Load
+
+        ' Disables input into the customer ID textbox
+        txtCustomerID.Enabled = False
+
+        If Dir$("Customers.txt") = "" Then
+
+            Dim sw As New StreamWriter("Customers.txt", True)
+
+            sw.WriteLine("0")
+
+            ' Needs to be close after use
+            sw.Close()
+
+            ' Warning that a new file has been created
+            MsgBox("A new file has been created", vbExclamation, "Warning!")
+
+        End If
+
+    End Sub
+
+    Private Sub cmdSave_Click(sender As System.Object, e As System.EventArgs) Handles cmdSave.Click
+
+        Dim CustomerData As New Customer
+
+        Dim CustomersData() As String = File.ReadAllLines(Dir$("Customers.txt"))
+
+        ' Lowest an ID can be
+        Dim CurrentCustomerID As Integer = 1
+
+        ' Clear the customer ID textbox
+        txtCustomerID.Text = ""
+
+        ' If the user is in read mode
+        If chkSaveMode.Checked = False Then
+
+            MsgBox("You must tick save mode if you would like to save data.")
+
+            Exit Sub
+
+        End If
+
+        ' i starts off at zero and incremements until it reaches the upper bound of customer data
+        For i = 0 To UBound(CustomersData)
+
+            ' If the highest ID is equal to the current customer ID, add one to it
+            If Val(Trim(Mid(CustomersData(i), 1, 4))) = CurrentCustomerID Then
+
+                ' Increment the value of the current customer ID
+                CurrentCustomerID = CurrentCustomerID + 1
+
+            End If
+
+        Next
+
+        ' If the data passed the test for validation
+        If Validation(CustomersData) = True Then
+
+            Dim sw As New System.IO.StreamWriter("Customers.txt", True)
+
+            ' The Customer ID is padded and saved to the structure
+            CustomerData.CustomerID = LSet(CurrentCustomerID, 4)
+
+            ' THE ID is then saved to the textbox for the user to view
+            txtCustomerID.Text = CurrentCustomerID
+
+            ' The data can now be saved as it's in the correct format
+
+            CustomerData.FirstName = LSet(txtFirstName.Text, 50)
+            CustomerData.Surname = LSet(txtEmailAddress.Text, 50)
+            CustomerData.EmailAddress = LSet(txtSurname.Text, 50)
+            CustomerData.PhoneNumber = LSet(txtPhoneNumber.Text, 50)
+
+            ' Write the data to a textfile
+            sw.WriteLine(CustomerData.CustomerID & CustomerData.FirstName & CustomerData.Surname & CustomerData.EmailAddress & CustomerData.PhoneNumber)
+
+            sw.Close()
+
+            ' Ouput that the file is saved with the customer ID
+            MsgBox("File Saved! Customer ID: " & CustomerData.CustomerID)
+
+        End If
+
+    End Sub
+
+    Private Sub cmdSearch_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmdSearch.Click
+
+        ' If the user is in save mode
+        If chkSaveMode.Checked = True Then
+
+            MsgBox("You must untick save mode if you would like to search data.")
+
+            Exit Sub
+
+        End If
+
+        Dim CustomerData() As String = File.ReadAllLines("Customers.txt")
+
+        Dim CustomerFound As Boolean
+        Dim CustomerCount As Integer
+        Dim FoundCustomer As Integer
+
+        CustomerFound = False
+        CustomerCount = 0
+
+        ' i starts off at zero and incremements until it reaches the upper bound of customer data
+        For i = 0 To UBound(CustomerData)
+
+            ' If the customer is searching a customer ID
+            If txtCustomerID.Text <> "" Then
+
+                If Trim(Mid(CustomerData(i), 1, 4)) = txtCustomerID.Text Then
+
+                    MsgBox("A customer with this Customer ID has been found.")
+
+                    ' A customer has been found
+                    CustomerFound = True
+
+                    ' Output the customer's data to the textboxes
+                    txtCustomerID.Text = Trim(Mid(CustomerData(i), 1, 4))
+                    txtFirstName.Text = Trim(Mid(CustomerData(i), 5, 50))
+                    txtSurname.Text = Trim(Mid(CustomerData(i), 55, 50))
+                    txtEmailAddress.Text = Trim(Mid(CustomerData(i), 105, 50))
+                    txtPhoneNumber.Text = Trim(Mid(CustomerData(i), 155, 50))
+
+                End If
+
+            Else
+
+                ' If the data in the textfile match the textboxes
+                If (Trim(Mid(CustomerData(i), 5, 50)) = txtFirstName.Text Or txtFirstName.Text = "") And (Trim(Mid(CustomerData(i), 55, 50)) = txtSurname.Text Or txtSurname.Text = "") And (Trim(Mid(CustomerData(i), 105, 50)) = txtEmailAddress.Text Or txtEmailAddress.Text = "") And (Trim(Mid(CustomerData(i), 155, 50)) = txtPhoneNumber.Text Or txtPhoneNumber.Text = "") Then
+
+                    ' A customer has been found, and the data is stored in the variable
+                    FoundCustomer = i
+
+                    ' Incrememnt number of customers
+                    CustomerCount = CustomerCount + 1
+
+                End If
+
+            End If
+
+        Next i
+
+        ' If the user is not searching for a customer ID
+        If txtCustomerID.Text = "" Then
+
+            If CustomerCount = 1 Then
+
+                MsgBox("One customer was found.")
+
+                ' Output the customer's data to the textboxes
+                txtCustomerID.Text = Trim(Mid(CustomerData(FoundCustomer), 1, 4))
+                txtFirstName.Text = Trim(Mid(CustomerData(FoundCustomer), 5, 50))
+                txtSurname.Text = Trim(Mid(CustomerData(FoundCustomer), 55, 50))
+                txtEmailAddress.Text = Trim(Mid(CustomerData(FoundCustomer), 105, 50))
+                txtPhoneNumber.Text = Trim(Mid(CustomerData(FoundCustomer), 155, 50))
+
+                Exit Sub
+
+            Else
+
+                MsgBox("There were " & CustomerCount & " customers found.")
+
+            End If
+
+        End If
+
+        ' If the user is searching for customer ID and a customer has not been found
+        If txtCustomerID.Text <> "" And CustomerFound = False Then
+
+            MsgBox("A customer with this Customer ID has not been found.")
+
+            ClearTextboxes()
+
+        End If
+
+    End Sub
+
+    Private Sub chkSaveMode_CheckedChanged(sender As System.Object, e As System.EventArgs) Handles chkSaveMode.Click
+
+        ' If the user is in save mode, otherwise in read mode
+        If chkSaveMode.Checked = True Then
+
+            ' User can now save
+            MsgBox("Save Mode enabled.")
+
+            ClearTextboxes()
+
+            ' Customer ID textbox can not be changed as they are in save mode
+            txtCustomerID.Enabled = False
+
+        Else
+
+            ' User can now search
+            MsgBox("Search Mode enabled.")
+
+            ClearTextboxes()
+
+            ' Customer ID textbox can be changed to allow the user to search
+            txtCustomerID.Enabled = True
+
+        End If
+
+    End Sub
+
+    Private Function Validation(CustomersData)
+
+        Dim Validated As Boolean
+
+        Validated = True
+
+        ' If any of the textboxes are blank, other than customer ID as this is generated
+        ' If phone number isn't numeric or it's not 11 characters
+        If txtFirstName.Text = "" Or txtSurname.Text = "" Or txtEmailAddress.Text = "" Or txtPhoneNumber.Text = "" Then
+
+            ' Data failed validation
+            Validated = False
+
+            MsgBox("You must save a customer's full name, email address and phone number.")
+
+        ElseIf (IsNumeric(txtPhoneNumber.Text) = False) Or (Val(txtPhoneNumber.Text.Length) <> 11) Then
+
+            ' Data failed validation
+            Validated = False
+
+            MsgBox("Phone number must consist of 11 numbers.")
+
+        End If
+
+        ' i starts off at zero and incremements until it reaches the upper bound of customer data
+        For i = 0 To UBound(CustomersData)
+
+            ' If all the data entered matches a set of data previously entered
+            If Trim(Mid(CustomersData(i), 5, 50)) = txtFirstName.Text And Trim(Mid(CustomersData(i), 55, 50)) = txtSurname.Text And Trim(Mid(CustomersData(i), 105, 50)) = txtEmailAddress.Text And Trim(Mid(CustomersData(i), 155, 50)) = txtPhoneNumber.Text Then
+
+                ' Data failed validation
+                Validated = False
+
+                MsgBox("This data has already been saved previously.")
+
+                Exit For
+
+            End If
+
+        Next
+
+        ' Return true or false on whether the data is validated
+        Return Validated
+
+    End Function
+
+    Private Sub ClearTextboxes()
+
+        ' Clears all the textboxes
+
+        txtCustomerID.Text = ""
+        txtFirstName.Text = ""
+        txtSurname.Text = ""
+        txtEmailAddress.Text = ""
+        txtPhoneNumber.Text = ""
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As System.Object, e As System.EventArgs) Handles btnClear.Click
+
+        ' Called when the user clicks the 'Clear' button
+        ClearTextboxes()
+
+    End Sub
+
+    Private Sub Panel1_Paint(sender As System.Object, e As System.Windows.Forms.PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+End Class
